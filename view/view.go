c99be6a88b92ab1282d3c7c4478cf811bcd526be
DQYuan <932087612@qq.com>
2019-10-13 20:00:23 +0800

    cross domain

--- view/view.go
+++ view/view.go
@@ -87,6 +87,9 @@ func Graph(yy string) (http.HandlerFunc, error) {
 	}
 
 	return func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
+		writer.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
+		writer.Header().Set("content-type", "application/json")             //返回数据格式是json
 		writer.Write(jsonBytes)
 	}, nil
 }
